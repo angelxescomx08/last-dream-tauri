713c4a4a393693b0e1f5a554c268c893dd436966
ahxdevelopmx <91965647+ahxdevelopmx@users.noreply.github.com>
2025-01-31 20:56:50 -0600

    enemies

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,16 @@ function App() {
         layout={{ type: "2D", rows: 10, columns: 10 }}
         animation2D={{ mode: "row", index: 0 }}
         fps={10}
+        scale={3}
+      />
+
+      <SpriteAnimator
+        spriteUrl="/characters/enemies/1.png"
+        frameWidth={122}
+        frameHeight={110}
+        layout={{ type: "2D", rows: 2, columns: 4 }}
+        animation2D={{ mode: "all" }}
+        fps={10}
         scale={2}
       />
 
